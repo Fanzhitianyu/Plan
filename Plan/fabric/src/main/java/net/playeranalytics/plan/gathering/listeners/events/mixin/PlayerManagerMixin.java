e4eb734e2f9af60b1cfc248fb66bce565a628b89
Fanzhitianyu <fanzhiyuxian@163.com>
2024-08-30 21:19:41 +0800

    1

--- Plan/fabric/src/main/java/net/playeranalytics/plan/gathering/listeners/events/mixin/PlayerManagerMixin.java
+++ Plan/fabric/src/main/java/net/playeranalytics/plan/gathering/listeners/events/mixin/PlayerManagerMixin.java
@@ -21,10 +21,12 @@ import net.minecraft.server.PlayerManager;
 import net.minecraft.text.Text;
 import net.playeranalytics.plan.gathering.listeners.events.PlanFabricEvents;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 
 @Mixin(PlayerManager.class)
@@ -32,7 +34,17 @@ public class PlayerManagerMixin {
 
     @Inject(method = "checkCanJoin", at = @At(value = "TAIL"))
     public void onLogin(SocketAddress address, GameProfile profile, CallbackInfoReturnable<Text> cir) {
-        PlanFabricEvents.ON_LOGIN.invoker().onLogin(address, profile, cir.getReturnValue());
+//        if(isLocalhost(address))
+            PlanFabricEvents.ON_LOGIN.invoker().onLogin(address, profile, cir.getReturnValue());
     }
+
+//    @Unique
+//    private static boolean isLocalhost(SocketAddress socketAddress) {
+//        if (socketAddress instanceof InetSocketAddress inetSocketAddress) {
+//            String hostAddress = inetSocketAddress.getAddress().getHostAddress();
+//            return "127.0.0.1".equals(hostAddress) || "::1".equals(hostAddress) || "localhost".equals(hostAddress);
+//        }
+//        return false;
+//    }
 
 }
